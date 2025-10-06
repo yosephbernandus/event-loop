c76d402d8cb2260fff5541306ad6d18c9514bac2
yosephbernandus <yosephbernandus@gmail.com>
2025-10-06 22:12:03 +0700

    liburing not available ubuntu 20.04

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Event loop project with liburing";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            liburing
+          ];
+
+          shellHook = ''
+            echo "Event loop development environment"
+            echo "liburing version: $(pkg-config --modversion liburing 2>/dev/null || echo 'installed')"
+          '';
+        };
+      });
+}
